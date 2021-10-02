0095ecb3b867f0cb118e481e280baac8989f51f6
Kris Litman <kris.d.litman@gmail.com>
2021-10-01 22:08:21 -0400

    Added user model test, all model tests and validations passing

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :reference_id, presence: true
+  validates :configuration, presence: true
+end
--- spec/models/sessions_spec.rb
+++ spec/models/sessions_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe User, type: :model do
+RSpec.describe Session, type: :model do
   context "Validations" do
     it { should validate_presence_of :user_id }
   end
