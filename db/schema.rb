18728747588403712fe5af1f8704cd356e3ab697
Kris Litman <kris.d.litman@gmail.com>
2021-10-01 21:18:41 -0400

    Added sessions through migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_011533) do
+ActiveRecord::Schema.define(version: 2021_10_02_011805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "reference_id"
