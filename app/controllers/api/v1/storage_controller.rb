c747b1f9e3fc82ecc7e8f9f5a717cb4300acf7c8
Kris Litman <kris.d.litman@gmail.com>
2021-10-01 21:43:19 -0400

    First sad path test passing

--- app/controllers/api/v1/storage_controller.rb
+++ app/controllers/api/v1/storage_controller.rb
@@ -1,20 +1,22 @@
 class Api::V1::StorageController < ApplicationController
   def create
-    what_to_store = params.dig(:args,:type)
-    case
-    when what_to_store == "store_user_and_session"
-      user = params.dig(:args,:user)
-      session = params.dig(:args,:session)
-      user_response = create_user(user)
-      session_response = create_session(session)
-      if user_response[:message].errors.nil? && user_response[:user]
-        user, session = user_response[:user], session_response[:session]
-        render StorageSerializer.new(user, session)
-      else
-        message = user_response[:message]
-        render ErrorSerializer.new(message)
+      what_to_store = params.dig(:args,:type)
+      case
+      when what_to_store == "store_user_and_session"
+        user = params.dig(:args,:user)
+        session = params.dig(:args,:session)
+        user_response = create_user(user)
+        session_response = create_session(session)
+        if user_response[:message].errors.nil? && user_response[:user]
+          user, session = user_response[:user], session_response[:session]
+          render StorageSerializer.new(user, session)
+        else
+          message = user_response[:message]
+          render json: message, status: 400
+        end
+      when !what_to_store
+        render json: { message: 'No parameters given' }, status: 400
       end
-    end
   end
 
   private
--- spec/v1/request/users/store_user_and_sessions_spec.rb
+++ spec/v1/request/users/store_user_and_sessions_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe "api/v1/store", type: :request do
   context "Only accepts POST requests" do
-    it "Can store objects" do
+    it "Without arguements request will fail" do
       post "/api/v1/store"
 
-      expect(response).to be_successful
-      expect(response.status).to eq(201)
+      expect(response).not_to be_successful
+      expect(response.status).to eq(400)
     end
   end
 end
