961494a3738e918f42ab52697be7c1502571efc9
Kris Litman <kris.d.litman@gmail.com>
2022-04-24 15:49:14 -0400

    Added coffee shop migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_24_190410) do
+ActiveRecord::Schema.define(version: 2022_04_24_194829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coffee_shops", force: :cascade do |t|
+    t.string "yelp_id"
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.string "rating"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_coffee_shops_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id"
@@ -41,5 +52,6 @@ ActiveRecord::Schema.define(version: 2022_04_24_190410) do
     t.string "zipcode"
   end
 
+  add_foreign_key "coffee_shops", "users"
   add_foreign_key "storage_logs", "users"
 end
---
+++ spec/v1/request/user_log/favorite_coffee_shop_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe "Coffee Shop object and Storage Log created when a User favorites a Coffee Shop", type: :request do
+	after(:all) do
+		StorageLog.destroy_all
+		CoffeeShop.destroy_all
+		User.destroy_all
+	end
+
+	setup do
+		@headers = {'CONTENT_TYPE' => 'application/json',
+			'ACCEPT' => 'application/json'}
+		@params = {
+			"created_at"=>"2022-04-23 00:34:43 -0400",
+			"payload"=>{
+				"type"=>"user",
+				"event"=>"create",
+				"id"=>"1234",
+				"first_name"=>"Y A2eb Yyz Xz",
+				"last_name"=>"Ua Cl Ft Fjt0",
+				"username"=>"O3FnjPaqki",
+				"email"=>"guy@example.com",
+				"zipcode"=>"JKGCWTg1QT"}
+		}
+	end
+
+	context "POST api/v1/user_log" do
+		it "Is not successful with no parameters" do
+			post api_v1_user_log_path, headers: @headers, params: {}
+
+			expect(response).not_to be_successful
+			expect(response.status).to eq(400)
+		end
+
+		it "Is successful with valid parameters" do
+			post api_v1_user_log_path, headers: @headers, params: @params.to_json
+			parsed = JSON.parse(response.body, symbolize_names: true)
+
+			expect(response).to be_successful
+			expect(response.status).to eq(201)
+			expect(parsed).to be_a(Hash)
+		end
+	end
+end
