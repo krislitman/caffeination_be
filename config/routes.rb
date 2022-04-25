cf1ba6b7f44bc00cb6d7829d367407e2b5393952
Kris Litman <kris.d.litman@gmail.com>
2022-04-24 23:12:53 -0400

    Refactored

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,16 @@ Rails.application.routes.draw do
 
 			# Favorite
 			post "/favorite", to: "coffee_shop#create"
+			delete "/favorite", to: "coffee_shop#destroy"
 
 			# Find User
 			get "/user", to: "user#find"
 
 			# Find User Favorites
 			get "/user_favorites", to: "user#find_favorites"
+
+			# Find If Shop is Favorited
+			get "/coffee_shop_favorite", to: "coffee_shop#favorite_check"
 		end
 	end
 end
